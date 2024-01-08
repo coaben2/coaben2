f833f5451ba337a6c1dca2b53b44df2d215b1291
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-08 16:34:24 +0100

    Initial commit with Vite/Vue 3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'Home' }">Accueil</RouterLink>
+        <RouterLink :to="{ name: 'GuildWars' }">Guild Wars 2</RouterLink>
+        <RouterLink :to="{ name: 'Blog' }">Blog</RouterLink>
+        <RouterLink :to="{ name: 'Portfolio' }">Portfolio</RouterLink>
+        <RouterLink :to="{ name: 'Contact' }">Contact</RouterLink>
+        <RouterLink :to="{ name: 'Login' }">Connexion</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/GuildWarsView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Guild Wars 2</h1>
+</template>
