0084c6b388dace1dba51a446a5b20369db40ad67
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-13 13:07:16 +0100

    Updated top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+
+import MainNavigation from './components/MainNavigation.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink :to="{ name: 'Home' }">Accueil</RouterLink>
-        <RouterLink :to="{ name: 'GuildWars' }">Guild Wars 2</RouterLink>
-        <RouterLink :to="{ name: 'Blog' }">Blog</RouterLink>
-        <RouterLink :to="{ name: 'Portfolio' }">Portfolio</RouterLink>
-        <RouterLink :to="{ name: 'Contact' }">Contact</RouterLink>
-        <RouterLink :to="{ name: 'Login' }">Connexion</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <MainNavigation />
+  <div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header class="bg-base-100">
+        <nav class="p-4 flex flex-start gap-4">
+            <RouterLink :to="{ name: 'Home' }" class="btn">Accueil</RouterLink>
+            <RouterLink :to="{ name: 'GuildWars' }" class="btn">Guild Wars 2</RouterLink>
+            <RouterLink :to="{ name: 'Blog' }" class="btn">Blog</RouterLink>
+            <RouterLink :to="{ name: 'Portfolio' }" class="btn">Portfolio</RouterLink>
+            <RouterLink :to="{ name: 'Contact' }" class="btn">Contact</RouterLink>
+            <RouterLink :to="{ name: 'Login' }" class="btn">Connexion</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header nav a:not(.router-link-active) {
+    @apply btn-primary;
+}
+
+header nav a.router-link-active {
+    @apply btn-secondary;
+}
+</style>
