6f5166fc29f18ec5beb4a286d2cf1c5f3eb63b3b
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-13 13:24:19 +0100

    Added Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router';
 
-import MainNavigation from './components/MainNavigation.vue';
+import AppNavigation from './components/AppNavigation.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
-  <MainNavigation />
-  <div>
+  <AppNavigation />
+  <div class="flex-1">
     <RouterView />
   </div>
+  <AppFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,42 @@
+<script setup>
+import InstagramIcon from '@/components/icons/InstagramIcon.vue';
+import TwitchIcon from '@/components/icons/TwitchIcon.vue';
+import YoutubeIcon from '@/components/icons/YoutubeIcon.vue';
+</script>
+
+<template>
+    <footer class="p-4 bg-base-100 flex gap-4 justify-between items-center">
+        <div class="text-sm">&copy; Coaben</div>
+        <ul class="social">
+            <li>
+                <a href="https://www.youtube.com/@coaben" target="_blank" rel="nofollow">
+                    <YoutubeIcon />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/coaben2" target="_blank" rel="nofollow">
+                    <InstagramIcon />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.twitch.tv/coaben" target="_blank" rel="nofollow">
+                    <TwitchIcon />
+                </a>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<style scoped>
+.social {
+    @apply flex gap-2;
+}
+
+.social li a {
+    @apply btn btn-circle;
+}
+
+.social li a svg {
+    @apply w-5 h-5;
+}
+</style>
