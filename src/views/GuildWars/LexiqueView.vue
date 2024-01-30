78eeed88904d05adb10595b1ec4d7872b67c4476
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-30 11:58:51 +0100

    Fixed typo H2>h2

--- src/views/GuildWars/LexiqueView.vue
+++ src/views/GuildWars/LexiqueView.vue
@@ -1,183 +1,156 @@
 <template>
-  <div>
-    <h2>lexique Guild wars 2</h2>
-    <p>merci a Kerhara pour ces informations
-
-    </p>
+  <div class="container max-w-2xl mx-auto bg-black bg-opacity-75 p-6 pb-2 md:rounded-lg md:my-6">
+    <h1>Lexique Guild wars 2</h1>
+    <p>merci a Kerhara pour ces informations</p>
     <h2>Fractal/frac :</h2>
-    <p>
-      instance pour groupe de 5 personnes. Sorte de donjon qui à une difficulté progressive
-    </p>
+    <p>instance pour groupe de 5 personnes. Sorte de donjon qui à une difficulté progressive</p>
     <h2>RA ou AR</h2>
     <p>
-      Amélioration que l'on obtient en faisant des fractals et que l'on place sur des emplacement de l'équipement élevé.
-      <br>150 de RA sont nécéssaire pour faire la fractal niveaux 100.
-      <br>On conseil en général de mettre des +9 pour atteindre ce nombre.
+      Amélioration que l'on obtient en faisant des fractals et que l'on place sur des emplacement de
+      l'équipement élevé.
+      <br />150 de RA sont nécéssaire pour faire la fractal niveaux 100. <br />On conseil en général
+      de mettre des +9 pour atteindre ce nombre.
 
-      <br>18 emplacement * +9 = 162 de RA
+      <br />18 emplacement * +9 = 162 de RA
 
-      <br>T1 : 1 à 25
-      <br>Niveau de difficulté 20+ = 25 AR
-      <br>T2 : 26 à 50
-      <br>Difficulté 30+ palier = 40 AR
-      <br>Difficulté 40+ palier = 55 AR
-      <br>Difficulté 50+ palier = 70 AR
-      <br>T3 : 51 à 75
-      <br>Niveau de difficulté 60+ = 85 AR
-      <br>Difficulté 70+ palier = 100 AR
-      <br>T4 : 76 à 100
-      <br>Difficulté 80+ palier = 115 AR
-      <br>Niveau de difficulté 90+ = 130 AR
-      <br>Niveau de difficulté 100 = 145 AR
+      <br />T1 : 1 à 25 <br />Niveau de difficulté 20+ = 25 AR <br />T2 : 26 à 50 <br />Difficulté
+      30+ palier = 40 AR <br />Difficulté 40+ palier = 55 AR <br />Difficulté 50+ palier = 70 AR
+      <br />T3 : 51 à 75 <br />Niveau de difficulté 60+ = 85 AR <br />Difficulté 70+ palier = 100 AR
+      <br />T4 : 76 à 100 <br />Difficulté 80+ palier = 115 AR <br />Niveau de difficulté 90+ = 130
+      AR <br />Niveau de difficulté 100 = 145 AR
     </p>
     <h2>CM (Challenge Mode) :</h2>
     <p>
       Mode de jeu que les joueurs choisissent d'activer.
-      <br>Disponible dans les dernieres fractal et sur certains boss de raid.
-      <br>Ajoute de la difficulté de jeu avec plus de méchanique, de Point de Vie ect...
+      <br />Disponible dans les dernieres fractal et sur certains boss de raid. <br />Ajoute de la
+      difficulté de jeu avec plus de méchanique, de Point de Vie ect...
     </p>
     <h2>LL, ley line, Ano, Anomaly, Naked man, homme tout nue :</h2>
     <p>
       Différentes appellations pour le boss de l'Anomalie des lignes de force.
-      <br>C'est le boss qui pop toutes les 2h et qui donne 1 Pièce mystique/jour/compte.
+      <br />C'est le boss qui pop toutes les 2h et qui donne 1 Pièce mystique/jour/compte.
     </p>
     <h2>MC ou Mytic Coin :</h2>
     <p>
       C'est les pièces mystique
-      <br>Item qui sert a crafter toute sorte de légendaire.
-      <br>avec SOTO on peut en obtenir dans la tour du sorcier
+      <br />Item qui sert a crafter toute sorte de légendaire. <br />avec SOTO on peut en obtenir
+      dans la tour du sorcier
     </p>
     <h2>CCDLN ou BLTC</h2>
-    <p>
-      hotel de vente (Compagnie Commercial du Lion Noir ou Black Lion Trading Company)
-    </p>
+    <p>hotel de vente (Compagnie Commercial du Lion Noir ou Black Lion Trading Company)</p>
     <h2>Spé Core :</h2>
     <p>
       C'est lorsque l'on joue une classe avec uniquement les builds (arbres de talents) vanilla.
-      <br>Sans les arbres de spécialisation des classes.
+      <br />Sans les arbres de spécialisation des classes.
     </p>
     <h2>TT ou Triple Terreur :</h2>
     <p>
-      World boss composer de trois boss guivres à tuer en simultanée qui requière beaucoup de coordination.
-      <br>Rapporte 2po, potentiellement du stuff élevée (?),
-      <br>ainsi que beaucoup de point de succès et des points de maîtrise de tyrie central.
+      World boss composer de trois boss guivres à tuer en simultanée qui requière beaucoup de
+      coordination.
+      <br />Rapporte 2po, potentiellement du stuff élevée (?), <br />ainsi que beaucoup de point de
+      succès et des points de maîtrise de tyrie central.
     </p>
     <h2>CC ou Crowd Control :</h2>
     <p>
       Tout sorts appliquant du contrôle.
-      <br>On parle de soft ou hard CC.
-      <br>On les utilisent pour casser la barre de défiance (ou la barre verte/bleu).
+      <br />On parle de soft ou hard CC. <br />On les utilisent pour casser la barre de défiance (ou
+      la barre verte/bleu).
 
-      <br>Effets qui appliquent des contrôles immédiat :
-      <br>Étourdissement, Stupeur, Attirer, Eclatement, Repoussement, Flottaison, Couler, Renversement.
-      <br>On privilégie bien évidemment les CC immédiat.
+      <br />Effets qui appliquent des contrôles immédiat : <br />Étourdissement, Stupeur, Attirer,
+      Eclatement, Repoussement, Flottaison, Couler, Renversement. <br />On privilégie bien
+      évidemment les CC immédiat.
 
-      <br>Effets qui appliquent des contrôles sur la durée:
-      <br>Peur, Incitation, Immobilisation, Lenteur, Givre, Aveuglement, Faiblesse, Infirmité.
+      <br />Effets qui appliquent des contrôles sur la durée: <br />Peur, Incitation,
+      Immobilisation, Lenteur, Givre, Aveuglement, Faiblesse, Infirmité.
     </p>
     <h2>RIBA , SW</h2>
     <p>
       Appellations des différentes escouades de farm sur la carte Contrée sauvages d'argent.
-      <br>A trouver dans le lfg : Tyrie central/escouade/monde vivant.
+      <br />A trouver dans le lfg : Tyrie central/escouade/monde vivant.
     </p>
-    <H2>LFG (Look for Group)</H2>
+    <h2>LFG (Look for Group)</h2>
+    <p>C'est tout simplement l'abréviation pour la Recherche de groupe en version anglais.</p>
+    <h2>BL : Black Liste</h2>
     <p>
-      C'est tout simplement l'abréviation pour la Recherche de groupe en version anglais.
-    </p>
-    <H2> BL : Black Liste</H2>
-    <p>
-      Liste, les gens que tu n'a pas envie de recroiser tellement ils t'ont laissé une mauvaise impssion.
-      <br>Pense à laisser un mot qui explique pourquoi ils sont BL.
+      Liste, les gens que tu n'a pas envie de recroiser tellement ils t'ont laissé une mauvaise
+      impssion.
+      <br />Pense à laisser un mot qui explique pourquoi ils sont BL.
     </p>
     <h2>Décrypter les message du LFG en raid</h2>
     <p>Par exemple : "W1-4 fc |250li| hfb, alac, qfb, cdps"</p>
     <p>
       Les lfg sont souvent constitués de plusieurs parties.
-      <br>La 1ere partie "W1-4 fc" indique les ailes de raids concernées.
-      <br>On traduirait ca par "ailes 1 à 4 Full Clear (tous les boss)".
-      <br>On s'engage donc à participer à un raid qui vas faire toutes les ailes de la 1 à la 4.
-      <br>On peut voir aussi le cas de "W1 fc @gors" ou "w3 fc @xera".
-      <br>Le @boss indique que l'escouade est dans l'aile indiquée mais à déjà clear tous les boss avant celui indiqué.
-      <br>@gors= le 1er boss (Vale Guardian) est déjà tué, @xera= il n'y a plus que xera à tuer.
-      <br>La deuxième partie concerne les conditions d'entrées dans l'escouade. Ici : "|250li|".
-      <br>On demande aux personnes qui souhaitent rejoindre l'escouade de justifier d'un certains nombre de LI en sa
-      <br>possession (250.)
-      <br>- Soit par ping dans le chat (on peut ping des LI directement, ou bien des parties d'armure légendaire,
-      <br>car elles en nécessite l'utilisation),
-      <br>- un code KPme (le site KillProofMe est un site sur lequel on peut enregistrer sa clef API gw2,
-      <br>et on peut ainsi y voir un résumé de ce que l'on a déjà tombé en raid et/ou fractal),
-      <br>- enfin peut aussi annoncer que l'on possède le "title" (lorsque l'on a terminer les succès des raids,
-      <br>on obtient un titre qui fait office de puve de nos compétences en raid).
-      <br>Il peut aussi être demandé des KP par exemple "50kp".
-      <br>Même principe que pour des LI mais avec les Kill Proof,
-      <br>qui sont les morceaux de décorations que l'on obtient lorsque l'on tue un boss (entre 1 à 5 par boss).
+      <br />La 1ere partie "W1-4 fc" indique les ailes de raids concernées. <br />On traduirait ca
+      par "ailes 1 à 4 Full Clear (tous les boss)". <br />On s'engage donc à participer à un raid
+      qui vas faire toutes les ailes de la 1 à la 4. <br />On peut voir aussi le cas de "W1 fc
+      @gors" ou "w3 fc @xera". <br />Le @boss indique que l'escouade est dans l'aile indiquée mais à
+      déjà clear tous les boss avant celui indiqué. <br />@gors= le 1er boss (Vale Guardian) est
+      déjà tué, @xera= il n'y a plus que xera à tuer. <br />La deuxième partie concerne les
+      conditions d'entrées dans l'escouade. Ici : "|250li|". <br />On demande aux personnes qui
+      souhaitent rejoindre l'escouade de justifier d'un certains nombre de LI en sa <br />possession
+      (250.) <br />- Soit par ping dans le chat (on peut ping des LI directement, ou bien des
+      parties d'armure légendaire, <br />car elles en nécessite l'utilisation), <br />- un code KPme
+      (le site KillProofMe est un site sur lequel on peut enregistrer sa clef API gw2, <br />et on
+      peut ainsi y voir un résumé de ce que l'on a déjà tombé en raid et/ou fractal), <br />- enfin
+      peut aussi annoncer que l'on possède le "title" (lorsque l'on a terminer les succès des raids,
+      <br />on obtient un titre qui fait office de puve de nos compétences en raid). <br />Il peut
+      aussi être demandé des KP par exemple "50kp". <br />Même principe que pour des LI mais avec
+      les Kill Proof, <br />qui sont les morceaux de décorations que l'on obtient lorsque l'on tue
+      un boss (entre 1 à 5 par boss).
     </p>
-    <H2>liste des rôles/classes recherchés.</H2>
+    <h2>liste des rôles/classes recherchés.</h2>
     <p>
-      <br>- hfb (pour Heal FireBrand),
-      <br>- qfb (pour Quick FireBrand),
-      <br>- alac (n'importe quelle classe pouvant donner le boon alac en groupe),
-      <br>- heal (n'importe quelle classe pouvant heal),
-      <br>- dps (n'importe quelle classe pouvant dps power ou condi),
-      <br>- tank (n'importe quelle classe pour tanker. Necessite soit de la robustesse soit connaitre la mechanique),
-      <br>- bs (pour Banner Slave, les guerriers utilisant les bannieres donnent un buff sur 10 personnes /!\ vouer à
-      <br>disparaitres avec le rework des bannieres),
-      <br>- quick (nimporte quelle classe pouvant apporter le boon quick en groupe),
-      <br>- dudu (pour Druide),
-      <br>- cdps (Condition dps, n'importe quelle classe pouvant dps condition),
-      <br>- hk (Rôle = Hand Kite, pour mechanique sur deimos),
-      <br>- canon (Rôle, pour mechanique des cannons sur sabetha par e xemple),
-      <br>- kite (Rôle, mechanique qui consiste à courir/rester éloigné du groupe),
-      <br>- push (Rôle, mechanique qui consiste a pousser qlqchose),
-      <br>- tower (Rôle, mechanique de la w3 sur l'escorte),
-      <br>ect...
+      <br />- hfb (pour Heal FireBrand), <br />- qfb (pour Quick FireBrand), <br />- alac (n'importe
+      quelle classe pouvant donner le boon alac en groupe), <br />- heal (n'importe quelle classe
+      pouvant heal), <br />- dps (n'importe quelle classe pouvant dps power ou condi), <br />- tank
+      (n'importe quelle classe pour tanker. Necessite soit de la robustesse soit connaitre la
+      mechanique), <br />- bs (pour Banner Slave, les guerriers utilisant les bannieres donnent un
+      buff sur 10 personnes /!\ vouer à <br />disparaitres avec le rework des bannieres), <br />-
+      quick (nimporte quelle classe pouvant apporter le boon quick en groupe), <br />- dudu (pour
+      Druide), <br />- cdps (Condition dps, n'importe quelle classe pouvant dps condition), <br />-
+      hk (Rôle = Hand Kite, pour mechanique sur deimos), <br />- canon (Rôle, pour mechanique des
+      cannons sur sabetha par e xemple), <br />- kite (Rôle, mechanique qui consiste à courir/rester
+      éloigné du groupe), <br />- push (Rôle, mechanique qui consiste a pousser qlqchose), <br />-
+      tower (Rôle, mechanique de la w3 sur l'escorte), <br />ect...
 
-      <br>- cDPS/pDPS : DPS c'est le diminutif de Damage Per Second (de l'anglais bien évidement).
-      <br>Quand on parle de dps on parle donc des dégâts que le personnage est capable d'infliger.
-      <br>En général on cherche a avoir le plus de dégâts par seconde pour tuer les mobs/boss très vite.
-      <br>- Cdps est la précision pour : Condi DPS. En français ce sont les altérations.
-      <br>Dans d'autres mmo on appel souvent ca des DOTs (pour Damage Over Time).
-      <br>Ce sont donc les builds qui basent leur dps sur l'application de poison, brulure, confusion, saignement,
-      tourment.
-      <br>- Pdps c'est pour : Power dps. Ce sont les builds basé sur les dégâts directe,
-      <br>autrement dit chaque coup porté doit infliger le plus de dégâts possible et plus on tape vite.
+      <br />- cDPS/pDPS : DPS c'est le diminutif de Damage Per Second (de l'anglais bien évidement).
+      <br />Quand on parle de dps on parle donc des dégâts que le personnage est capable d'infliger.
+      <br />En général on cherche a avoir le plus de dégâts par seconde pour tuer les mobs/boss très
+      vite. <br />- Cdps est la précision pour : Condi DPS. En français ce sont les altérations.
+      <br />Dans d'autres mmo on appel souvent ca des DOTs (pour Damage Over Time). <br />Ce sont
+      donc les builds qui basent leur dps sur l'application de poison, brulure, confusion,
+      saignement, tourment. <br />- Pdps c'est pour : Power dps. Ce sont les builds basé sur les
+      dégâts directe, <br />autrement dit chaque coup porté doit infliger le plus de dégâts possible
+      et plus on tape vite.
 
-      <br>- HAM pour Heal Alac Mechanist (ou jambon pour le jeu de mot en Français) :
-      <br>C'est la spécialisation orienté soin de la 3eme extension de la classe ingénieur.
+      <br />- HAM pour Heal Alac Mechanist (ou jambon pour le jeu de mot en Français) : <br />C'est
+      la spécialisation orienté soin de la 3eme extension de la classe ingénieur.
 
-      <br>- BU ou Back Up :Personne qui repnd le rôle/mécanique si le principal concerné meurt.
+      <br />- BU ou Back Up :Personne qui repnd le rôle/mécanique si le principal concerné meurt.
     </p>
     <h2>Strike ou mission d'attaque</h2>
     <p>Instance PvE de 10 personnes</p>
     <h2>liste des strike</h2>
     <p>
-      <br>IBS (IceBrood Saga)
+      <br />IBS (IceBrood Saga)
 
-      <br>1-Col des Cimefroides
-      <br>2-Voix et Griffe légendaires des déchus
-      <br>3-Fraenir de Jormag
-      <br>4-Désosseur
-      <br>5-Murmure de Jormag
-      <br>6-Forge de l'acier
-      <br>7-Guerre froide
+      <br />1-Col des Cimefroides <br />2-Voix et Griffe légendaires des déchus <br />3-Fraenir de
+      Jormag <br />4-Désosseur <br />5-Murmure de Jormag <br />6-Forge de l'acier <br />7-Guerre
+      froide
 
-      <br>dans le LFG il y auras souvant des groupe pour IBS3 pour ne faire que les 3 pmières strikes
-      <br>qui sont les plus simple
+      <br />dans le LFG il y auras souvant des groupe pour IBS3 pour ne faire que les 3 pmières
+      strikes <br />qui sont les plus simple
     </p>
     <p>
-      <br>EOD (end of dragon)
+      <br />EOD (end of dragon)
 
-      <br>1-Antre étherlame
-      <br>2-Décharge de Xunlai Jade
-      <br>3-Belvédère de Kaineng
-      <br>4-Temple des moissons
-      <br>5-Cour du vieux Lion
+      <br />1-Antre étherlame <br />2-Décharge de Xunlai Jade <br />3-Belvédère de Kaineng
+      <br />4-Temple des moissons <br />5-Cour du vieux Lion
     </p>
     <p>
-      <br>SOTO (Secrets of the Obscure)
+      <br />SOTO (Secrets of the Obscure)
 
-      <br>1-Observatoire cosmique
-      <br>2-Temple de Febe
+      <br />1-Observatoire cosmique <br />2-Temple de Febe
     </p>
   </div>
 </template>
