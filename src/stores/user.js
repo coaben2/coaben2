4070acab6295f416dd33cfa75698ea55ad980e99
coaben <149945920+coaben2@users.noreply.github.com>
2024-02-21 21:44:42 +0100

    maj character + ajout tanstack

---
+++ src/stores/user.js
@@ -0,0 +1,109 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+const APIURL = 'https://api.guildwars2.com/v2';
+
+export const useUserStore = defineStore('user', () => {
+    const apiKey = ref(null);
+    const newApiKey = ref(null);
+    const haveApiKey = ref(false);
+    const error = ref(null);
+
+    const checkApiKey = async () => {
+        error.value = null;
+        if (!newApiKey.value.length) return;
+        try {
+            const req = await fetch(`${APIURL}/account?access_token=${newApiKey.value}`);
+            const res = await req.json();
+            if (!req.ok) {
+                if (res.text) {
+                    throw new Error(res.text);
+                }
+                throw new Error(`GW2 API error ${req.status}`);
+            }
+            return true;
+        } catch (err) {
+            error.value = err.message;
+            return false;
+        }
+    };
+
+    const saveApiKey = () => {
+        localStorage.setItem('gw2-api-key', newApiKey.value);
+        apiKey.value = newApiKey.value;
+        haveApiKey.value = true;
+    };
+
+    const initApiKey = () => {
+        const localApiKey = localStorage.getItem('gw2-api-key');
+        if (localApiKey) {
+            apiKey.value = localApiKey;
+            haveApiKey.value = true;
+        }
+    };
+
+    const setApiKey = async (token) => {
+        newApiKey.value = token;
+        if (await checkApiKey()) {
+            saveApiKey();
+        }
+    };
+
+    const getApiKey = () => {
+        return apiKey.value;
+    };
+
+    const getCharacters = () => {
+        return fetch(`${APIURL}/characters?access_token=${apiKey.value}&`).then((res) => res.json());
+    };
+
+    const getCharacter = (name) => {
+        return fetch(`${APIURL}/characters/${name}?access_token=${apiKey.value}`).then((res) =>
+            res.json(),
+        );
+    };
+
+    const getBank = () => {
+        return fetch(`${APIURL}/bank?access_token=${apiKey.value}&`).then((res) => res.json());
+    };
+
+    const getMatérials = () => {
+        return fetch(`${APIURL}/account/materials?access_token=${apiKey.value}&`).then((res) => res.json());
+    };
+
+    const getItems = () => {
+        return fetch(`${APIURL}/items?`).then((res) => res.json());
+    };
+
+    const getItemsDetails = function (t) {
+        var i = t.toString();
+        fetch("https://api.guildwars2.com/v2/items?lang=fr&ids=" + i)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(item => {
+                    const itemLink = $("#itemLinks").val();
+                    $(`tr[data-itemid='${item.id}'] .icon img`).attr("data-src", item.icon);
+                    $(`tr[data-itemid='${item.id}'] .item-name`).addClass(item.rarity).html(`<a href="${itemLink}${item.id}" target="_blank">${item.name}</a>`);
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+
+    const getRecipes = () => {
+        return fetch(`${APIURL}/recipes?`).then((res) => res.json());
+    };
+
+    const getSkins = () => {
+        return fetch(`${APIURL}/skins?`).then((res) => res.json());
+    };
+
+    const getColors = () => {
+        return fetch(`${APIURL}/colors?`).then((res) => res.json());
+    };
+
+    initApiKey();
+
+    return { setApiKey, haveApiKey, error, getApiKey, getCharacters, getCharacter };
+});
--- src/views/GuildWars/Api/CharactersView.vue
+++ src/views/GuildWars/Api/CharactersView.vue
@@ -1,5 +1,163 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { useUserStore } from '@/stores/user';
+import { useQuery, useMutation } from '@tanstack/vue-query';
+
+const user = useUserStore();
+
+const haveApiKey = computed(() => !!user.haveApiKey);
+
+const currentCharacter = ref(null);
+const loadingMessage = ref(null);
+
+const getCharacters = async () => {
+    loadingMessage.value = 'Chargement de la liste des personnages';
+
+    const characters = await user.getCharacters();
+    if (!characters) return false;
+
+    currentCharacter.value = characters[0];
+    handleGetCharacter();
+    return characters;
+};
+
+const getCharacter = () => {
+    loadingMessage.value = `Chargement du personnage : ${currentCharacter.value}`;
+    return user.getCharacter(currentCharacter.value);
+};
+
+const { isLoading: isCharactersLoading, data: charactersData } = useQuery({
+    queryKey: ['characters'],
+    queryFn: getCharacters,
+    enabled: haveApiKey,
+});
+
+const {
+    isPending: isCharacterPending,
+    data: characterData,
+    mutate: handleGetCharacter,
+} = useMutation({
+    mutationFn: () => getCharacter(),
+});
+</script>
+
 <template>
-    <div>
-        <h2>Personnages</h2>
+    <div class="flex-container">
+        <div class="flex-item" v-if="charactersData">
+            <select v-model="currentCharacter" @change="handleGetCharacter" class="select select-bordered">
+                <option v-for="character in charactersData" :key="character" :value="character">
+                    {{ character }}
+                </option>
+            </select>
+            <p>Race: {{ characterData.race }}</p>
+            <p>genre: {{ characterData.gender }}</p>
+            <p>Profession: {{ characterData.profession }}</p>
+            <p>niveau: {{ characterData.level }}</p>
+            <H3 class="mb-6">métiers :</H3>
+            <table class="bordered-table">
+                <thead>
+                    <tr>
+                        <th>Métier</th>
+                        <th>Niveau</th>
+                        <th>état</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="craftingItem in characterData.crafting" :key="craftingItem.discipline">
+                        <td>{{ craftingItem.discipline }}</td>
+                        <td>{{ craftingItem.rating }}</td>
+                        <td>{{ craftingItem.active ? 'Actif' : 'Inactif' }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="flex-container">
+            <div class="flex-equipement" v-if="characterData && characterData.equipment">
+                <div class="column-container">
+                    <div v-for="equipmentItem in characterData.equipment" :key="equipmentItem.slot" class="column">
+                        <div>
+                            <img :src="'https://v2.lebusmagique.fr/img/api/items/' + equipmentItem.id + '.png'"
+                                :alt="equipmentItem.id" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- modif visuel voir si on peut faire comme dans le jeux les 6 pieces de stuf / les 4 armes-->
+        <!-- le do les 2 accessoires-->
+        <!-- amulette les 2 anneaux-->
+        <!-- la relique-->
+        <!--item de récoltes-->
+        <!--item du drone-->
+        <!--items de pêche-->
+        <div class="flex-item" v-if="characterData && characterData.bags">
+            <h2>sac :</h2>
+            <div v-for="bags in characterData.bags" :key="bags.id">
+                <h3>{{ bags.slot }}</h3>
+                <ul>
+                    <li>{{ bags.id }}</li>
+                    <li>
+                        <ul>
+                            <li v-for="item in bags.contents" :key="item.id">
+                                {{ item.id }}
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
+
+
+    <div class="rounded bg-opacity-50 bg-black p-4 my-4 flex gap-2 items-center"
+        v-if="isCharactersLoading || isCharacterPending">
+        <span class="loading loading-spinner text-primary"></span>
+        <span>{{ loadingMessage }}</span>
+    </div>
+
+    <pre v-if="characterData">{{ characterData }}</pre>
 </template>
+
+<style scoped>
+.flex-container {
+    display: flex;
+}
+
+.flex-item {
+    flex: 2;
+    margin-right: 20px;
+}
+
+.flex-equipement {
+    flex: 4;
+    margin-right: 20px;
+}
+
+.bordered-table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+.bordered-table th,
+.bordered-table td {
+    border: 1px solid black;
+
+    padding: 6px;
+    text-align: left;
+}
+
+.column-container {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.column {
+    width: 25%;
+    box-sizing: border-box;
+    padding: 5px;
+}
+
+img {
+    max-width: 100%;
+}
+</style>
